2474462667868500acc7cb5e30f5532da0ca697e
Jeffrey Maxwell <>
2020-03-19 16:29:45 -0700

    Some attempt at socket code

--- ClassBoards/app/src/main/java/com/example/classboards/MainActivity.java
+++ ClassBoards/app/src/main/java/com/example/classboards/MainActivity.java
@@ -13,16 +13,22 @@ public class MainActivity extends AppCompatActivity {
 
     private static String HOST_NAME = "attu6.cs.washington.edu";
     private static int PORT_NUM = 12345;
+    private static Socket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Socket server = setup();
+        this.socket = setup();
         if (server == null) {
             Log.e("error", "null server");
         }
-
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                receivingThread();
+            }
+        }).start();
 
     }
 
@@ -42,13 +48,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void ReceivingThread() {
+    public void onClassSelect() {
+        Button courseButton = (Button) findViewById(r.id.course_textbox);
+        String course = courseButton.getText().toString();
+        String msg = "update: " + course;
+        OutputStream out = new DataOutputStream(new BufferedOutputStream(this.socket.getOutputStream()));
+        out.write(msg);
+    }
 
+    public void onMessageSend() {
+        Button messageButton = (Button) findViewById(r.id.message_textbox);
+        String msg = messageButton.getText().toString();
+        Button nameButton = (Button) findViewById(r.id.name_textboox);
+        String name = nameButton.getText().toString();
+        String signed_msg = name + ": " + msg;
+        OutputStream out = new DataOutputStream(new BufferedOutputStream(this.socket.getOutputStream()));
+        out.write(signed_msg);
+    }
+
+    public void receivingThread() {
+        InputStream in = new DataInputStream(new BufferedInputStream(this.socket.getInputStream()));
         while (true) {
-
-
+            byte[] buffer = new byte[8192]; // or 4096, or more
+            in.read(buffer);
+            String chat_text = new String(buffer, "UTF-8");
+            TextView chatbox = (TextView) findViewById(r.id.textView2);
+            chatbox.setText(chat_text);
         }
-
 
     }
 
